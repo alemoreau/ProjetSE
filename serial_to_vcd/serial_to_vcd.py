c6fbcd3e64f1b437ddb517bb07fdb708aaae319d
klem33 <clement.sanquer@gmail.com>
2015-12-15 22:17:02 +0100

    perfectionnement serial.c et serial to vcd

---
+++ serial_to_vcd/serial_to_vcd.py
@@ -0,0 +1,60 @@
+import serial
+import time
+
+ser = serial.Serial(
+    port='COM3',\
+    baudrate=2000000,\
+    parity=serial.PARITY_NONE,\
+    stopbits=serial.STOPBITS_TWO,\
+    bytesize=serial.EIGHTBITS,\
+    timeout=1)
+
+value = 0x00;
+
+#print (ser)
+
+print "$date"
+print "   Coucou :)"
+print "$end"
+print "$version"
+print "   Coucou :)"
+print "$end"
+print "$comment"
+print "   Any comment text."
+print "$end"
+print "$timescale 1ps $end"
+print "$scope module logic $end"
+print "$var wire 1 A data $end"
+print "$var wire 1 B clock $end"
+print "$upscope $end"
+print "$enddefinitions $end"
+print "$dumpvars"
+out = ""
+current_time = 0
+start_time = 0
+data = -1
+clock = -1 
+
+temps = 1;
+
+while 1:
+    donnee=ser.read(size=1)
+
+    if(donnee != ""):
+    	value =ord(donnee)
+
+        current_time = time.time() * 1000000
+        #if start_time == 0:
+        #    start_time = current_time
+        print "#" + str(temps)
+        temps +=1  
+        #str(int(current_time - start_time))
+        # Display data change (if any)
+        if value & 0x01 != data:
+            data = value & 0x01
+            print str(data)+"A"     #data
+        if (value & 0x02) >> 1 != clock:
+            clock = (value & 0x02) >> 1
+            print str(clock)+"B"    #clock
+
+    	#print "%X" % value,
